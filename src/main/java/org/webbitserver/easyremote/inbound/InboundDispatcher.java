c9def4d2ff0a205ac61d96260f3e1779cededb40
Joe Walnes <joe@walnes.com>
2011-02-14 12:03:35 +0000

    Refactored InboundDispatcher so actions are computed at initialization time

--- src/main/java/org/webbitserver/easyremote/inbound/InboundDispatcher.java
+++ src/main/java/org/webbitserver/easyremote/inbound/InboundDispatcher.java
@@ -5,7 +5,6 @@ import org.webbitserver.HttpRequest;
 import org.webbitserver.WebSocketConnection;
 import org.webbitserver.easyremote.Remote;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,50 +12,67 @@ import java.util.Set;
 
 public class InboundDispatcher {
 
-    public static class ActionArgsTuple {
+    private final Gson gson;
+    private final Map<String, Action> inboundActions = new HashMap<String, Action>();
+
+    public InboundDispatcher(Gson gson, final Object server, final Class<?> clientType) {
+        this.gson = gson;
+
+        for (final Method method : server.getClass().getMethods()) {
+            if (method.getAnnotation(Remote.class) != null) {
+                inboundActions.put(method.getName(), new ReflectiveAction(method, clientType, server));
+            }
+        }
+    }
+
+    private static class ActionArgsTuple {
         public String action;
         public Object[] args;
     }
 
-    private final Gson gson;
-    private final Object server;
-    private final Class<?> clientType;
-    private final Map<String, Method> serverMethods = new HashMap<String, Method>();
-
-    public InboundDispatcher(Gson gson, Object server, Class<?> clientType) {
-        this.gson = gson;
-        this.server = server;
-        this.clientType = clientType;
-        for (Method method : server.getClass().getMethods()) {
-            if (method.getAnnotation(Remote.class) != null) {
-                serverMethods.put(method.getName(), method);
-            }
-        }
-    }
-
-    public void dispatch(WebSocketConnection connection, String msg, Object client) throws IllegalAccessException, InvocationTargetException {
+    public void dispatch(WebSocketConnection connection, String msg, Object client) throws Exception {
         ActionArgsTuple map = gson.fromJson(msg, ActionArgsTuple.class);
-        Method method = serverMethods.get(map.action);
-
-        Class<?>[] paramTypes = method.getParameterTypes();
-        Object[] args = new Object[paramTypes.length];
-        int argIndex = 0;
-        for (int i = 0; i < paramTypes.length; i++) {
-            Class<?> paramType = paramTypes[i];
-            if (paramType.isAssignableFrom(clientType)) {
-                args[i] = client;
-            } else if (paramType.isAssignableFrom(WebSocketConnection.class)) {
-                args[i] = connection;
-            } else if (paramType.isAssignableFrom(HttpRequest.class)) {
-                args[i] = connection.httpRequest();
-            } else {
-                args[i] = map.args[argIndex++];
-            }
-        }
-        method.invoke(server, args);
+        Action action = inboundActions.get(map.action);
+        action.call(connection, client, map.args);
     }
 
     public Set<String> availableMethods() {
-        return serverMethods.keySet();
+        return inboundActions.keySet();
+    }
+
+    private static interface Action {
+        void call(WebSocketConnection connection, Object client, Object[] args) throws Exception;
+    }
+
+    private static class ReflectiveAction implements Action {
+        private final Method method;
+        private final Class<?> clientType;
+        private final Object server;
+
+        public ReflectiveAction(Method method, Class<?> clientType, Object server) {
+            this.method = method;
+            this.clientType = clientType;
+            this.server = server;
+        }
+
+        @Override
+        public void call(WebSocketConnection connection, Object client, Object[] args) throws Exception {
+            Class<?>[] paramTypes = method.getParameterTypes();
+            Object[] callArgs = new Object[paramTypes.length];
+            int argIndex = 0;
+            for (int i = 0; i < paramTypes.length; i++) {
+                Class<?> paramType = paramTypes[i];
+                if (paramType.isAssignableFrom(clientType)) {
+                    callArgs[i] = client;
+                } else if (paramType.isAssignableFrom(WebSocketConnection.class)) {
+                    callArgs[i] = connection;
+                } else if (paramType.isAssignableFrom(HttpRequest.class)) {
+                    callArgs[i] = connection.httpRequest();
+                } else {
+                    callArgs[i] = args[argIndex++];
+                }
+            }
+            method.invoke(server, callArgs);
+        }
     }
 }
