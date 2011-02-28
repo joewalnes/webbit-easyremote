bd07b7e2e54cb3cb8c8ffe966fb685edc8bda6d3
Joe Walnes <joe@truemesh.com>
2011-02-28 15:35:06 -0800

    Allow WebbitSocket.on{error,close,open} to be hooked up.

--- src/main/java/org/webbitserver/easyremote/magic.js
+++ src/main/java/org/webbitserver/easyremote/magic.js
@@ -3,8 +3,15 @@ function WebbitSocket(path, target) {
     var ws = new WebSocket('ws://' + document.location.host + path);
     ws.onclose = function() {
         target.onclose && target.onclose();
+        self.onclose && self.onclose();
+    };
+    ws.onerror = function() {
+        target.onerror && target.onerror();
+        self.onerror && self.onerror();
     };
     ws.onmessage = function(e) {
+        target.onmessage && target.onmessage(e);
+        self.onmessage && self.onmessage(e);
         var msg = JSON.parse(e.data);
         if (msg.exports) {
             msg.exports.forEach(function(name) {
@@ -17,6 +24,7 @@ function WebbitSocket(path, target) {
                 };
             })
             target.onopen && target.onopen();
+            self.onopen && self.onopen();
         } else {
             var action = target[msg.action];
             if (typeof action === 'function') {
